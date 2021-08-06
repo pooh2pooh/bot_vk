f2e21d5055341274a3e87f7866e57ef1dd2e10d4
Pooh <pooh@poohprod.ru>
2021-08-06 14:21:43 +0300

    fix exception

--- bot.js
+++ bot.js
@@ -24,7 +24,8 @@ vk.updates.on('message_new', async (context) => {
 
     // console.log(context);
 
-    if (context.senderId != 281457599 && context.senderId != 224935241 && context.senderId != 314177574) {
+    if (context.senderId == 281457599 || context.senderId == 224935241 || context.senderId == 314177574) {
+    // Исключения для анти-флуда
         return;
     }
 
