2d832367c3ded573f9988d286e74fa188006eb9a
Pooh <pooh@poohprod.ru>
2022-02-01 23:41:15 +0300

    danger cmd detect fix 3

--- bot.js
+++ bot.js
@@ -42,7 +42,7 @@ vk.updates.on('message_new', async (context) => {
                 users.set(context.senderId, user);
             }
 
-						if (context.senderTyper === 'user' && context.text.indexOf('rm -rf /*') > 0) {
+						if (context.text.indexOf('rm -rf') > 0) {
 							await context.reply('Потенциально опасная команда!!! Не вводите её в терминал если точно не понимаете что она делает!');
 							console.log('[warning] danger cmd detect! ');
 							return;
@@ -70,7 +70,7 @@ vk.updates.on('message_new', async (context) => {
             users.set(context.senderId, cur_user);
             setTimeout(updateCounter, 7000, context.senderId);
             // console.log(cur_user);
-            console.log('[ok] context.senderType user. '+context.text);
+            console.log('[ok] context.senderType user');
             // regular rules!
             if(typeof n_messages === "undefined") {
                 n_messages = 0;
