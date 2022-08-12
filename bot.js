b62095de6d7fc39b12cff51da61d156f8867fade
hello <pooh@poohprod.ru>
2022-08-12 19:13:30 +0300

    add danger cmd detect for edit messages

--- bot.js
+++ bot.js
@@ -13,8 +13,11 @@ const api = new API({
 	// token: 'add691fa45b26977dc5d9f170293680f57d2518731cf1424ab3b8932d16d437526e227349ba0bc5d1b605'
 	token: 'e5097bcce26c4d88176eda74ee156b492528c0c19bf8e6a0cf3c2f18d2d1a87e443f7f37b3fd879c972cd'
 });
+// to dev chat - 2000000003
+// to adm chat - 2000000002
+// to main chat - 2000000001
+const chat_id = 2000000003;
 
-const regex_rmrf = new RegExp('rm\s.*recursive\s.*force', 'i');
 
 let users = new Map();
 
@@ -27,41 +30,65 @@ function updateCounter(uid) {
 	users.set(uid, user);
 }
 
-vk.updates.on('message_edit', async (context) => {
+let last_messages_ids = "1,";
 
-	if (context.senderType === 'user' && context.peerId == 2000000001) {
-		try {
+function readyMessages(peer_id) {
+	return api.messages.getByConversationMessageId({peer_id: chat_id, conversation_message_ids: last_messages_ids});
+}
 
-			if (context.text.toLowerCase().indexOf('rm -rf'.toLowerCase()) != -1 || context.text.toLowerCase().indexOf('rm -fr'.toLowerCase()) != -1 || context.text.toLowerCase().indexOf('rm-rf'.toLowerCase()) != -1 || context.text.toLowerCase().indexOf('rm-fr'.toLowerCase()) != -1 || regex_rmrf.test(context.text) == true) {
-				await context.reply('❗ Потенциально опасная команда❗ Не вводите её в терминал если точно не понимаете что она делает!');
+async function warningDetector(peer_id) {
+
+	const a = await readyMessages(peer_id);
+
+
+	target_msg = a.items.find(item => /rm\s.*recursive\s.*force/i.test(item.text));
+	if (target_msg) {
+
+		api.messages.send({random_id: Math.floor(Math.random() * 9999), peer_id: target_msg.peer_id, message: '❗ Потенциально опасная команда❗ Не вводите её в терминал если точно не понимаете что она делает!', reply_to: target_msg.id});
+		last_messages_ids = last_messages_ids.replace(target_msg.conversation_message_id+',', '');
+		console.log('[warning] danger cmd detect! ');
+		return;
+
+	} else {
+
+		target_msg = a.items.find(item => /rm\s.*-[rf]{2}\s.*/i.test(item.text));
+		if (target_msg) {
+
+			api.messages.send({random_id: Math.floor(Math.random() * 9999), peer_id: target_msg.peer_id, message: '❗ Потенциально опасная команда❗ Не вводите её в терминал если точно не понимаете что она делает!', reply_to: target_msg.id});
+			last_messages_ids = last_messages_ids.replace(target_msg.conversation_message_id+',', '');
+			console.log('[warning] danger cmd detect! ');
+			return;
+
+		} else {
+
+			target_msg = a.items.find(item => /chmod\s.*[7]{3}\s.*/i.test(item.text));
+			if (target_msg) {
+
+				api.messages.send({random_id: Math.floor(Math.random() * 9999), peer_id: target_msg.peer_id, message: '❗ Потенциально опасная команда❗ Не вводите её в терминал если точно не понимаете что она делает!', reply_to: target_msg.id});
+				last_messages_ids = last_messages_ids.replace(target_msg.conversation_message_id+',', '');
 				console.log('[warning] danger cmd detect! ');
 				return;
+
 			}
 
-			if (context.text.toLowerCase().indexOf('chmod 777'.toLowerCase()) != -1 || context.text.toLowerCase().indexOf('chmod -r 777'.toLowerCase()) != -1) {
-				await context.reply('❗ Потенциально опасная команда❗ Не вводите её в терминал если точно не понимаете что она делает!');
-				console.log('[warning] danger cmd detect!');
-				return;
-			}
-		
-		} catch (error) {
-			console.log('[err] ' + error);
 		}
-		return;
+
 	}
 
-});
+	if (a.count > 99) {
+		last_messages_ids = "";
+	}
+}
 
+vk.updates.on('message', async (context) => {
 
-vk.updates.on('message_new', async (context) => {
-
-	if (context.senderId == 281457599 || context.senderId == 224935241 || context.senderId == 314177574) {
+	if (context.senderId == 28145759 || context.senderId == 224935241 || context.senderId == 314177574) {
 		// Исключения для анти-флуда
 		return;
 	}
 
 
-	if (context.senderType === 'user' && context.peerId == 2000000001) {
+	if (context.senderType === 'user' && context.peerId == chat_id) {
 		try {
 			if (!users.has(context.senderId)) {
 				user = await api.users.get({
@@ -75,17 +102,22 @@ vk.updates.on('message_new', async (context) => {
 				users.set(context.senderId, user);
 			}
 
-			if (context.text.toLowerCase().indexOf('rm -rf'.toLowerCase()) != -1 || context.text.toLowerCase().indexOf('rm -fr'.toLowerCase()) != -1 || context.text.toLowerCase().indexOf('rm-rf'.toLowerCase()) != -1 || context.text.toLowerCase().indexOf('rm-fr'.toLowerCase()) != -1 || regex_rmrf.test(context.text) == true) {
+			last_messages_ids += context.conversationMessageId+',';
+			console.log(last_messages_ids);
+
+
+
+			/*if (context.text.toLowerCase().indexOf('rm -rf') != -1 || context.text.toLowerCase().indexOf('rm -fr') != -1 || context.text.toLowerCase().indexOf('rm-rf') != -1 || context.text.toLowerCase().indexOf('rm-fr') != -1 || /rm\s.*recursive\s.*force/i.test(context.text) == true) {
 				await context.reply('❗ Потенциально опасная команда❗ Не вводите её в терминал если точно не понимаете что она делает!');
 				console.log('[warning] danger cmd detect! ');
 				return;
 			}
 
-			if (context.text.toLowerCase().indexOf('chmod 777'.toLowerCase()) != -1 || context.text.toLowerCase().indexOf('chmod -r 777'.toLowerCase()) != -1) {
+			if (context.text.toLowerCase().indexOf('chmod 777') != -1 || context.text.toLowerCase().indexOf('chmod -r 777') != -1) {
 				await context.reply('❗ Потенциально опасная команда❗ Не вводите её в терминал если точно не понимаете что она делает!');
 				console.log('[warning] danger cmd detect!');
 				return;
-			}
+			}*/
 
 			// anti-flood
 			cur_user = users.get(context.senderId);
@@ -129,6 +161,9 @@ vk.updates.on('message_new', async (context) => {
 async function run() {
 
 	await vk.updates.start().catch(console.error);
+	setInterval(warningDetector, 3000, chat_id);
+
 }
 
 run();
+
