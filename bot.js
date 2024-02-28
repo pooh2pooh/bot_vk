d9e8a867ba0c56cb2bdfa5dd2ad0ecce5842c7a8
hello <pooh@poohprod.ru>
2024-02-28 14:19:09 +0300

    Refactoring and fix issue

--- bot.js
+++ bot.js
@@ -7,6 +7,8 @@ let config = require('./token.json');
 const parser = new Parser();
 const vk = new VK(config);
 const api = new API(config);
+
+
 // dev chat - 2000000003
 // adm chat - 2000000002
 // main chat - 2000000001
@@ -14,6 +16,8 @@ const chat_id = 2000000003;
 
 let users = new Map();
 let last_messages_ids = "99,";
+
+
 let last_pub_date_rss = null;
 let last_pub_date_rss_news = null;
 let last_pub_date_rss_releases = null;
@@ -22,7 +26,15 @@ let last_pub_date_rss_testing = null;
 let last_pub_date_rss_unstable = null;
 
 
-function updateCounter(uid)
+// Период (в секундах) через который убавляется счётчик АНТИФЛУДа для пользователя
+const ANTIFLOOD_TIME = 7;
+// Сколько сообщений подряд от одного пользователя, будет считаться флудом
+const ANTIFLOOD_COUNTER = 4;
+
+
+// Убавляет счётчик АНТИФЛУДа для пользователя,
+// каждое сообщение пользователя вызывает эту функцию по таймеру
+async function updateCounter(uid)
 {
 	user = users.get(uid);
 	// console.log(user);
@@ -32,6 +44,7 @@ function updateCounter(uid)
 	users.set(uid, user);
 }
 
+
 function readyMessages(chat_id)
 {
 	return api.messages.getByConversationMessageId({
@@ -40,6 +53,8 @@ function readyMessages(chat_id)
 	});
 }
 
+// Ищет в сообщених опасные команды,
+// даже если отредактировано старое сообщение
 async function warningDetector(chat_id)
 {
 	const now = new Date();
@@ -78,144 +93,215 @@ async function warningDetector(chat_id)
 	}
 }
 
-async function getRSSFeed()
+
+async function get_rss_feed()
 {
 	const now = new Date();
 	const randomId = now.getTime();
 
 	(async () => {
-	  const feed = await parser.parseURL('https://blog.manjaro.org/feed/');
-	  // console.log(feed.title);
+		try {
+		  const feed = await parser.parseURL('https://blog.manjaro.org/feed/');
+		  // console.log(feed.title);
 	 
-	  feed.items.forEach(item => {
-	  	if (!last_pub_date_rss || new Date(item.isoDate) > last_pub_date_rss) {
-		  	api.messages.send({
-		  		random_id: randomId,
-		      peer_id: chat_id,
-		      message: '📗 Новая запись в блоге \n',
-		      attachment: [
-						item.link,
-					]
-	      });
-	      last_pub_date_rss = new Date(item.isoDate);
-	      fs.writeFileSync('feed_blog.txt', item.isoDate, 'utf8');
-	      // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
-	  	}
-	    // console.log(item.title + ': ' + item.link)
-	  });
+		  feed.items.forEach(item => {
+		  	if (!last_pub_date_rss || new Date(item.isoDate) > last_pub_date_rss) {
+			  	api.messages.send({
+			  		random_id: randomId,
+			      peer_id: chat_id,
+			      message: '📗 Новая запись в блоге \n',
+			      attachment: [
+							item.link,
+						]
+		      });
+		      last_pub_date_rss = new Date(item.isoDate);
+		      fs.writeFileSync('feed_blog.txt', item.isoDate, 'utf8');
+		      // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
+		  	}
+		    // console.log(item.title + ': ' + item.link)
+		  });
+	  } catch (error) {
+			console.log('[err] ' + error);
+		}
 	})();
+
+	//console.log('[ok]. Get RSS feed for Feed.');
+}
+
+
+async function get_rss_notices()
+{
+	const now = new Date();
+	const randomId = now.getTime();
 
 	(async () => {
-	  const feed = await parser.parseURL('https://forum.manjaro.org/c/notices.rss');
-	  // console.log(feed.title);
-	 
-	  feed.items.forEach(item => {
-	  	if (!last_pub_date_rss_news || new Date(item.isoDate) > last_pub_date_rss_news) {
-		  	api.messages.send({
-		  		random_id: randomId,
-		      peer_id: chat_id,
-		      message: '⚡ Важная заметка \n',
-		      attachment: [
-						item.link,
-					]
-	      });
-	      last_pub_date_rss_news = new Date(item.isoDate);
-	      fs.writeFileSync('feed_notices.txt', item.isoDate, 'utf8');
-	      // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
-	  	}
-	    // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
-	  });
+		try {
+		  const feed = await parser.parseURL('https://forum.manjaro.org/c/notices.rss');
+		  // console.log(feed.title);
+		 
+		  feed.items.forEach(item => {
+		  	if (!last_pub_date_rss_news || new Date(item.isoDate) > last_pub_date_rss_news) {
+			  	api.messages.send({
+			  		random_id: randomId,
+			      peer_id: chat_id,
+			      message: '⚡ Важная заметка \n',
+			      attachment: [
+							item.link,
+						]
+		      });
+		      last_pub_date_rss_news = new Date(item.isoDate);
+		      fs.writeFileSync('feed_notices.txt', item.isoDate, 'utf8');
+		      // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
+		  	}
+		    // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
+		  });
+		} catch (error) {
+			console.log('[err] ' + error);
+		}
 	})();
+
+	//console.log('[ok]. Get RSS feed for Notices.');
+}
+
+
+async function get_rss_releases()
+{
+	const now = new Date();
+	const randomId = now.getTime();
 
 	(async () => {
-	  const feed = await parser.parseURL('https://forum.manjaro.org/c/announcements/releases.rss');
-	  // console.log(feed.title);
-	 
-	  feed.items.forEach(item => {
-	  	if (!last_pub_date_rss_releases || new Date(item.isoDate) > last_pub_date_rss_releases) {
-		  	api.messages.send({
-		  		random_id: randomId,
-		      peer_id: chat_id,
-		      message: '👻 Новый релиз! \n',
-		      attachment: [
-						item.link,
-					]
-	      });
-	      last_pub_date_rss_releases = new Date(item.isoDate);
-	      fs.writeFileSync('feed_releases.txt', item.isoDate, 'utf8');
-	      // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
-	  	}
-	    // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
-	  });
+		try {
+		  const feed = await parser.parseURL('https://forum.manjaro.org/c/announcements/releases.rss');
+		  // console.log(feed.title);
+		 
+		  feed.items.forEach(item => {
+		  	if (!last_pub_date_rss_releases || new Date(item.isoDate) > last_pub_date_rss_releases) {
+			  	api.messages.send({
+			  		random_id: randomId,
+			      peer_id: chat_id,
+			      message: '👻 Новый релиз! \n',
+			      attachment: [
+							item.link,
+						]
+		      });
+		      last_pub_date_rss_releases = new Date(item.isoDate);
+		      fs.writeFileSync('feed_releases.txt', item.isoDate, 'utf8');
+		      // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
+		  	}
+		    // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
+		  });
+		} catch (error) {
+			console.log('[err] ' + error);
+		}
 	})();
+
+	//console.log('[ok]. Get RSS feed for Releases.');
+}
+
+
+async function get_rss_stable()
+{
+	const now = new Date();
+	const randomId = now.getTime();
 
 	(async () => {
-	  const feed = await parser.parseURL('https://forum.manjaro.org/c/announcements/stable-updates.rss');
-	  // console.log(feed.title);
-	 
-	  feed.items.forEach(item => {
-	  	if (!last_pub_date_rss_stable || new Date(item.isoDate) > last_pub_date_rss_stable) {
-		  	api.messages.send({
-		  		random_id: randomId,
-		      peer_id: chat_id,
-		      message: '✅ Стабильное обновление \n',
-		      attachment: [
-						item.link,
-					]
-	      });
-	      last_pub_date_rss_stable = new Date(item.isoDate);
-	      fs.writeFileSync('feed_stable.txt', item.isoDate, 'utf8');
-	      // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
-	  	}
-	    // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
-	  });
+		try {
+		  const feed = await parser.parseURL('https://forum.manjaro.org/c/announcements/stable-updates.rss');
+		  // console.log(feed.title);
+		 
+		  feed.items.forEach(item => {
+		  	if (!last_pub_date_rss_stable || new Date(item.isoDate) > last_pub_date_rss_stable) {
+			  	api.messages.send({
+			  		random_id: randomId,
+			      peer_id: chat_id,
+			      message: '✅ Стабильное обновление \n',
+			      attachment: [
+							item.link,
+						]
+		      });
+		      last_pub_date_rss_stable = new Date(item.isoDate);
+		      fs.writeFileSync('feed_stable.txt', item.isoDate, 'utf8');
+		      // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
+		  	}
+		    // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
+		  });
+	  } catch (error) {
+			console.log('[err] ' + error);
+		}
 	})();
+
+	//console.log('[ok]. Get RSS feed for Stable branch.');
+}
+
+
+async function get_rss_testing()
+{
+	const now = new Date();
+	const randomId = now.getTime();
 
 	(async () => {
-	  const feed = await parser.parseURL('https://forum.manjaro.org/c/announcements/testing-updates.rss');
-	  // console.log(feed.title);
-	 
-	  feed.items.forEach(item => {
-	  	if (!last_pub_date_rss_testing || new Date(item.isoDate) > last_pub_date_rss_testing) {
-		  	api.messages.send({
-		  		random_id: randomId,
-		      peer_id: chat_id,
-		      message: '⚠ Обновление тестовой ветки\n',
-		      attachment: [
-						item.link,
-					]
-	      });
-	      last_pub_date_rss_testing = new Date(item.isoDate);
-	      fs.writeFileSync('feed_testing.txt', item.isoDate, 'utf8');
-	      // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
-	  	}
-	    // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
-	  });
+		try {
+		  const feed = await parser.parseURL('https://forum.manjaro.org/c/announcements/testing-updates.rss');
+		  // console.log(feed.title);
+		 
+		  feed.items.forEach(item => {
+		  	if (!last_pub_date_rss_testing || new Date(item.isoDate) > last_pub_date_rss_testing) {
+			  	api.messages.send({
+			  		random_id: randomId,
+			      peer_id: chat_id,
+			      message: '⚠ Обновление тестовой ветки\n',
+			      attachment: [
+							item.link,
+						]
+		      });
+		      last_pub_date_rss_testing = new Date(item.isoDate);
+		      fs.writeFileSync('feed_testing.txt', item.isoDate, 'utf8');
+		      // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
+		  	}
+		    // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
+		  });
+	  } catch (error) {
+			console.log('[err] ' + error);
+		}
 	})();
 
-	(async () => {
-	  const feed = await parser.parseURL('https://forum.manjaro.org/c/announcements/unstable-updates.rss');
-	  // console.log(feed.title);
-	 
-	  feed.items.forEach(item => {
-	  	if (!last_pub_date_rss_unstable || new Date(item.isoDate) > last_pub_date_rss_unstable) {
-		  	api.messages.send({
-		  		random_id: randomId,
-		      peer_id: chat_id,
-		      message: '‼ Обновление нестабильной ветки\n',
-		      attachment: [
-						item.link,
-					]
-	      });
-	      last_pub_date_rss_unstable = new Date(item.isoDate);
-	      fs.writeFileSync('feed_unstable.txt', item.isoDate, 'utf8');
-	      // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
-	  	}
-	    // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
-	  });
-	})();
+	//console.log('[ok]. Get RSS feed for Testing branch.');
+}
 
-	// console.log('Okay. Get RSS feed.');
+
+async function get_rss_unstable()
+{
+	const now = new Date();
+	const randomId = now.getTime();
+
+	
+		(async () => {
+			try {
+			  const feed = await parser.parseURL('https://forum.manjaro.org/c/announcements/unstable-updates.rss');
+			  // console.log(feed.title);
+			 
+			  feed.items.forEach(item => {
+			  	if (!last_pub_date_rss_unstable || new Date(item.isoDate) > last_pub_date_rss_unstable) {
+				  	api.messages.send({
+				  		random_id: randomId,
+				      peer_id: chat_id,
+				      message: '‼ Обновление нестабильной ветки\n',
+				      attachment: [
+								item.link,
+							]
+			      });
+			      last_pub_date_rss_unstable = new Date(item.isoDate);
+			      fs.writeFileSync('feed_unstable.txt', item.isoDate, 'utf8');
+			      // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
+			  	}
+			    // console.log(item.isoDate + ' ' + item.title + ': ' + item.link)
+			  });
+		  } catch (error) {
+				console.log('[err] ' + error);
+			}
+		})();
+
+		//console.log('[ok] Get RSS feed for Unstable branch.');
 }
 
 
@@ -250,16 +336,16 @@ vk.updates.on('message', async (context) => {
 
 			// anti-flood
 			cur_user = users.get(context.senderId);
-			if (cur_user[1]['flood_counter'] < 5) {
+			if (cur_user[1]['flood_counter'] < ANTIFLOOD_COUNTER) {
 				cur_user[1]['flood_counter']++;
 			}
-			if (cur_user[1]['flood_counter'] == 5) {
+			if (cur_user[1]['flood_counter'] == ANTIFLOOD_COUNTER) {
 				cur_user[1]['warn_counter']++;
 				if (cur_user[1]['warn_counter'] == 2) {
 					// await context.reply('мут 1 минуту'); ## uncomment for mute action!
 					cur_user[1]['warn_counter'] = 0;
 					api.messages.removeChatUser({
-					  chat_id: context.peerId,
+					  chat_id: context.peerId - 2000000000,
 					  user_id: context.senderId,
 					});
 					console.log('[mute/kick] for ' + cur_user[0]['last_name']);
@@ -270,9 +356,10 @@ vk.updates.on('message', async (context) => {
 				// context.send();
 			}
 			users.set(context.senderId, cur_user);
-			setTimeout(updateCounter, 7000, context.senderId);
+			setTimeout(await updateCounter, ANTIFLOOD_TIME*1000, context.senderId);
 			// console.log(cur_user);
 			// console.log('[ok] context.senderType user');
+
 			// regular rules!
 			if (typeof n_messages === "undefined") {
 				n_messages = 0;
@@ -357,8 +444,14 @@ async function run()
 	  }
 	});
 
-	//setInterval(getRSSFeed, 6000);
+	setInterval(get_rss_feed, 6000);
+	setInterval(get_rss_notices, 6000);
+	setInterval(get_rss_releases, 6000);
+	setInterval(get_rss_stable, 6000);
+	setInterval(get_rss_testing, 6000);
+	setInterval(get_rss_unstable, 6000);
 
 }
 
 run();
+console.log('Бот запущен.');
